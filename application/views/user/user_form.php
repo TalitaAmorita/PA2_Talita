52f340893711764324d7f7a5f907ec8a107e9bda
TalitaAmorita <talitaamor29@2gmail.com>
2022-01-23 03:03:37 +0700

    Source Code PA2

---
+++ application/views/user/user_form.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+    <head>
+        <title>harviacode.com - codeigniter crud generator</title>
+        <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>"/>
+        <style>
+            body{
+                padding: 15px;
+            }
+        </style>
+    </head>
+    <body>
+        <h2 style="margin-top:0px">User <?php echo $button ?></h2>
+        <form action="<?php echo $action; ?>" method="post">
+	    <div class="form-group">
+            <label for="int">Id Role <?php echo form_error('id_role') ?></label>
+            <input type="text" class="form-control" name="id_role" id="id_role" placeholder="Id Role" value="<?php echo $id_role; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Nama <?php echo form_error('nama') ?></label>
+            <input type="text" class="form-control" name="nama" id="nama" placeholder="Nama" value="<?php echo $nama; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Alamat <?php echo form_error('alamat') ?></label>
+            <input type="text" class="form-control" name="alamat" id="alamat" placeholder="Alamat" value="<?php echo $alamat; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Jenis Kelamin <?php echo form_error('jenis_kelamin') ?></label>
+            <input type="text" class="form-control" name="jenis_kelamin" id="jenis_kelamin" placeholder="Jenis Kelamin" value="<?php echo $jenis_kelamin; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="date">Tgl Lahir <?php echo form_error('tgl_lahir') ?></label>
+            <input type="text" class="form-control" name="tgl_lahir" id="tgl_lahir" placeholder="Tgl Lahir" value="<?php echo $tgl_lahir; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="int">No Telp <?php echo form_error('no_telp') ?></label>
+            <input type="text" class="form-control" name="no_telp" id="no_telp" placeholder="No Telp" value="<?php echo $no_telp; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Email <?php echo form_error('email') ?></label>
+            <input type="text" class="form-control" name="email" id="email" placeholder="Email" value="<?php echo $email; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Password <?php echo form_error('password') ?></label>
+            <input type="text" class="form-control" name="password" id="password" placeholder="Password" value="<?php echo $password; ?>" />
+        </div>
+	    <input type="hidden" name="id_user" value="<?php echo $id_user; ?>" /> 
+	    <button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
+	    <a href="<?php echo site_url('user') ?>" class="btn btn-default">Cancel</a>
+	</form>
+    </body>
+</html>
